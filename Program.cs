5dcc6db97d58119a662db92b797f6027f836dc5c
Fernandolopez1111 <00223519@uca.edu.sv>
2021-03-10 21:39:42 -0600

    primer commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace gitlab
+{
+   internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0,b =0 ; 
+            Console.WriteLine("enter a value for a");
+            a = Int32.Parse(Console.ReadLine());
+            
+            Console.WriteLine("enter a value for b");
+            b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"adding {a} + {b} equals  {addNumbers(a,b)}");
+
+        }
+        public static int addNumbers(int a,int b)
+        {
+            return a + b;
+        }
+    }
+}
